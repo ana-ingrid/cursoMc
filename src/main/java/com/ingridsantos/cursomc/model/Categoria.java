326af1a906ea93a1f6dfddbca7411ebb073d760f
AnaIngrid <ingridsantoscosta2003@gmail.com>
2023-07-31 20:50:29 -0300

    Conexão com o banco de dados testada e classe Modelo Categoria criada

---
+++ src/main/java/com/ingridsantos/cursomc/model/Categoria.java
@@ -0,0 +1,58 @@
+package com.ingridsantos.cursomc.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria that = (Categoria) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    @Override
+    public String toString () {
+        return "Categorias{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+
+}
--- src/main/java/com/ingridsantos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ingridsantos/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,29 @@
 package com.ingridsantos.cursomc.resources;
 
+import com.ingridsantos.cursomc.model.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-//    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     @GetMapping("/{cpf}")
-    public String listar() {
-        return "Rest está funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 =  new Categoria(1, "informática");
+        Categoria cat2 =  new Categoria(2, "Escritório");
+
+        List<Categoria> listaCategoria = new ArrayList<>();
+        listaCategoria.add(cat1);
+        listaCategoria.add(cat2);
+
+        return listaCategoria;
     }
 
 }
