9257798506c2c5dac4d58017cb4101a3af4d7dce
AnaIngrid <ingridsantoscosta2003@gmail.com>
2023-09-03 22:30:12 -0300

    Inserindo método salvarCategoria com POST

--- src/main/java/com/ingridsantos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ingridsantos/cursomc/resources/CategoriaResource.java
@@ -3,10 +3,10 @@ package com.ingridsantos.cursomc.resources;
 import com.ingridsantos.cursomc.model.Categoria;
 import com.ingridsantos.cursomc.service.CategoriaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -22,5 +22,14 @@ public class CategoriaResource {
      ResponseEntity<Categoria> ConsultaCategoriaPorId(@PathVariable Integer id) {
         return ResponseEntity.status(200).body(categoriaService.ConsultaCategoriaPorId(id));
     }
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Categoria> salvaCategoria(@RequestBody Categoria obj){
+        obj = categoriaService.salvaCategoria(obj);
+         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+
 
 }
--- src/main/java/com/ingridsantos/cursomc/service/CategoriaService.java
+++ src/main/java/com/ingridsantos/cursomc/service/CategoriaService.java
@@ -3,9 +3,11 @@ package com.ingridsantos.cursomc.service;
 import com.ingridsantos.cursomc.exceptions.ObjetoNaoEncontradoException;
 import com.ingridsantos.cursomc.model.Categoria;
 import com.ingridsantos.cursomc.repository.CategoriaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Optional;
+import java.net.URI;
 
 @Service
 public class CategoriaService {
@@ -20,5 +22,8 @@ public class CategoriaService {
         return categoriaRepository.findById(id).orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado"));
     }
 
-
+    public Categoria salvaCategoria(Categoria obj){
+        obj.setId(null);
+        return categoriaRepository.save(obj);
+    }
 }
